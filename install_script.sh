d0a1aebf8f912166c051971363318c76f553ee72
Ahmed Fahim <afahim03@yahoo.com>
2025-04-21 00:43:36 +0600

    Update install script

--- install_script.sh
+++ install_script.sh
@@ -1,8 +1,8 @@
-if [ -f .env ]; then
-    source .env
-fi
+# if [ -f .env ]; then
+#     source .env
+# fi
 
-curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+# curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
 # az login --identity --username $MANAGED_ID_CLIENT_ID
 
@@ -10,8 +10,10 @@ mkdir data
 mkdir data/processed
 mkdir data/processed/MINDsmall_train
 
-az storage blob download --container-name $CONTAINER_NAME --account-name $BUCKET_NAME --file data/processed/MINDsmall_train/behaviors.parquet --name behaviors.parquet --auth-mode key --sas-token $BLOB_SAS_TOKEN
-az storage blob download --container-name $CONTAINER_NAME --account-name $BUCKET_NAME --file data/processed/MINDsmall_train/news_text.parquet --name news_text.parquet --auth-mode key --sas-token $BLOB_SAS_TOKEN
-az storage blob download --container-name $CONTAINER_NAME --account-name $BUCKET_NAME --file mydb_train.sqlite --name mydb_train.sqlite --auth-mode key --sas-token $BLOB_SAS_TOKEN
-# az storage blob download --account-name bucketlist --container-name mycontainer --file data/processed/MINDsmall_train/behaviors.parquet --name behaviors.parquet --auth-mode login
-# az storage blob download --account-name bucketlist --container-name mycontainer --file data/processed/MINDsmall_train/news_text.parquet --name news_text.parquet --auth-mode login
+mv ../mydb_train.sqlite ./
+mv ../behaviors.parquet data/processed/MINDsmall_train/
+mv ../news_text.parquet data/processed/MINDsmall_train/
+
+# az storage blob download --container-name $CONTAINER_NAME --account-name $BUCKET_NAME --file data/processed/MINDsmall_train/behaviors.parquet --name behaviors.parquet --auth-mode key --sas-token $BLOB_SAS_TOKEN
+# az storage blob download --container-name $CONTAINER_NAME --account-name $BUCKET_NAME --file data/processed/MINDsmall_train/news_text.parquet --name news_text.parquet --auth-mode key --sas-token $BLOB_SAS_TOKEN
+# az storage blob download --container-name $CONTAINER_NAME --account-name $BUCKET_NAME --file mydb_train.sqlite --name mydb_train.sqlite --auth-mode key --sas-token $BLOB_SAS_TOKEN
